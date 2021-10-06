0b090740a8cb10073cb57d3f6ae977fe0c905e0e
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2021-10-05 22:07:24 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
-
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+if not IY_LOADED then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end
 
 if IS_LOADED then
 	error("Infinite Store is already running!",0)
