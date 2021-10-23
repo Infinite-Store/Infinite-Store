f231623f59b1b600fa8e58cd37ce6d537abf2192
Toon <85719086+Toon-arch@users.noreply.github.com>
2021-10-23 00:26:15 -0400

    capitalism gang

--- main.lua
+++ main.lua
@@ -1436,7 +1436,7 @@ local guiSettings = {
 
 	["Safe Mode"] = {
 		["Name"] = "Safe Mode",
-		["Description"] = "Hides NSFW plugins.",
+		["Description"] = "Hides NSFW Plugins.",
 		["SettingFunction"] = function()
 			if _UserSettings.SafeMode == true then
 				checkBoxHandler(false, settingsList["SafeMode"].CheckBox)
