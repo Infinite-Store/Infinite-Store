a054f64f9042771ec204365e53798441b39c4394
Epikest <Epikest@users.noreply.github.com>
2022-06-29 20:59:13 +0000

    Commit from GitHub Actions (.github/workflows/format.yml)

--- plugins/nsfwplugins/sources.lua
+++ plugins/nsfwplugins/sources.lua
@@ -1,25 +1,49 @@
-return {	
+return {
 	["NsfwR6"] = {
-		["Name"] = "NsfwR6", ["Creator"] = "Bloxxy213", ["CreationDate"] = "Unknown", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/NsfwR6.iy", ["Commands"] = {"NSFWr6 / NSFW"},
-	};
-	
+		["Name"] = "NsfwR6",
+		["Creator"] = "Bloxxy213",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/NsfwR6.iy",
+		["Commands"] = { "NSFWr6 / NSFW" },
+	},
+
 	["Ass"] = {
-		["Name"] = "Ass", ["Creator"] = "Unknown", ["CreationDate"] = "Unknown", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/ass.iy", ["Commands"] = {"ass"},
-	};
-	
+		["Name"] = "Ass",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/ass.iy",
+		["Commands"] = { "ass" },
+	},
+
 	["BoobiesAndAss"] = {
-		["Name"] = "BoobiesAndAss", ["Creator"] = "Unknown", ["CreationDate"] = "Unknown", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/assandboobies.iy", ["Commands"] = {"boobiesandass"},
-	};
-	
+		["Name"] = "BoobiesAndAss",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/assandboobies.iy",
+		["Commands"] = { "boobiesandass" },
+	},
+
 	["Boobies"] = {
-		["Name"] = "Boobies", ["Creator"] = "NoobSploit", ["CreationDate"] = "08/10/2021", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/boobies.iy", ["Commands"] = {"boobs"},
-	};
-	
+		["Name"] = "Boobies",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "08/10/2021",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/boobies.iy",
+		["Commands"] = { "boobs" },
+	},
+
 	["Dick"] = {
-		["Name"] = "Dick", ["Creator"] = "Unknown", ["CreationDate"] = "Unknown", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/dick.iy", ["Commands"] = {"dick"},
-	};
+		["Name"] = "Dick",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/dick.iy",
+		["Commands"] = { "dick" },
+	},
 
 	["MoreSex"] = {
-		["Name"] = "MoreSex", ["Creator"] = "prisj", ["CreationDate"] = "09/30/2021", ["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/moresex.iy", ["Commands"] = {"getbang / gbang [plr]", "ungetbang / ungbang [plr]", "suck [plr]", "unsuck [plr]"},
-	};
+		["Name"] = "MoreSex",
+		["Creator"] = "prisj",
+		["CreationDate"] = "09/30/2021",
+		["GithubLink"] = "https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/plugins/moresex.iy",
+		["Commands"] = { "getbang / gbang [plr]", "ungetbang / ungbang [plr]", "suck [plr]", "unsuck [plr]" },
+	},
 }
