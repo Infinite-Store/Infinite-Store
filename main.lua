3fe9a8b90934ae3a9e0d53e20f68f3633517a503
Toon <85719086+Toon-arch@users.noreply.github.com>
2022-09-09 22:58:01 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 -- Temporary fix for people who still use the old loader
-loadstring(game:HttpGetAsync("https://cdn.jsdelivr.net/gh/Infinite-Store/Infinite-Store@main/main.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinite-Store/Infinite-Store/main/main.luau"))()
