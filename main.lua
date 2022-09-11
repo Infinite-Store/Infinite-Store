60a448d956a51f4076ea4d811538a37f31cffbf6
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2022-09-11 16:26:03 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,40 @@ if not IY_LOADED then
 	wait(0.75)
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KaizerFox/inviteme/main/invite.lua"))()
+if syn then
+		syn.request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+					["Content-Type"] = "application/json";
+					["Origin"] = "https://discord.com";
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+				["cmd"] = "INVITE_BROWSER";
+				["args"] = { ["code"] = "mVzBU7GTMy" }; -- put invite code 
+				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
+			})
+		})
+
+		else 
+
+		pcall(function()
+			http.request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+					["Content-Type"] = "application/json";
+					["Origin"] = "https://discord.com";
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+				["cmd"] = "INVITE_BROWSER";
+				["args"] = { ["code"] = "VwCGhNu9Rb" }; -- put invite code 
+				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
+			})
+		})
+		end)
+	end
+
 
 
 local IS_Settings = {
