660cc2de28766df4a7bf110a61ffa92779eabcc2
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2022-09-11 16:30:19 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,42 +6,6 @@ if not IY_LOADED then
 	wait(0.75)
 end
 
-if syn then
-		syn.request({
-			Url = "http://127.0.0.1:6463/rpc?v=1",
-			Method = "POST",
-			Headers = {
-					["Content-Type"] = "application/json";
-					["Origin"] = "https://discord.com";
-			},
-			Body = game:GetService("HttpService"):JSONEncode({
-				["cmd"] = "INVITE_BROWSER";
-				["args"] = { ["code"] = "mVzBU7GTMy" }; -- put invite code 
-				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
-			})
-		})
-
-		else 
-
-		pcall(function()
-			http.request({
-			Url = "http://127.0.0.1:6463/rpc?v=1",
-			Method = "POST",
-			Headers = {
-					["Content-Type"] = "application/json";
-					["Origin"] = "https://discord.com";
-			},
-			Body = game:GetService("HttpService"):JSONEncode({
-				["cmd"] = "INVITE_BROWSER";
-				["args"] = { ["code"] = "VwCGhNu9Rb" }; -- put invite code 
-				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
-			})
-		})
-		end)
-	end
-
-
-
 local IS_Settings = {
 	["Version"] = "1.3.6",
 	["Invite"] = "dv96SXYmxz",
@@ -51,7 +15,7 @@ local IS_Settings = {
 	),
 }
 
-local ann = "NEW DISCORD, JOIN PLEASE"
+local ann = "NEW DISCORD, JOIN PLEASE https://discord.gg/VwCGhNu9Rb"
 local _UserSettings = {
 	["StartMinimized"] = false,
 	["SafeMode"] = false,
