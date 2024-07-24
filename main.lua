d79ce58480cab1f5e32ed064023d099a5845d12c
Toon <85719086+Toon-arch@users.noreply.github.com>
2024-07-24 12:17:53 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
 -- security reasons
+-- plus infinite store is discontinued and no longer maintained don't use it
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
